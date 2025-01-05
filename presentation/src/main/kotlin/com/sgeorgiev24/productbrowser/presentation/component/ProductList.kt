3f71d6065ad9097e6979059d2c8b62a42fee95e8
Stanislav Georgiev <sgeorgiev24@gmail.com>
2025-01-05 18:32:15 +0200

    Support landscape for the product list

--- presentation/src/main/kotlin/com/sgeorgiev24/productbrowser/presentation/component/ProductList.kt
+++ presentation/src/main/kotlin/com/sgeorgiev24/productbrowser/presentation/component/ProductList.kt
@@ -1,15 +1,19 @@
 package com.sgeorgiev24.productbrowser.presentation.component
 
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import com.sgeorgiev24.productbrowser.domain.model.product.Product
 
@@ -19,8 +23,12 @@ fun ProductList(
     onFavoriteClick: (Product) -> Unit,
     onItemClick: (Product) -> Unit
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(vertical = 8.dp)
+    val configuration = LocalConfiguration.current
+    val isLandScape = configuration.orientation == ORIENTATION_LANDSCAPE
+
+    LazyVerticalGrid(
+        contentPadding = PaddingValues(vertical = 8.dp, horizontal = if (isLandScape) 12.dp else 0.dp),
+        columns = GridCells.Fixed(if (isLandScape) 3 else 1)
     ) {
         itemsIndexed(products ?: emptyList()) { index, product ->
             ProductItem(
