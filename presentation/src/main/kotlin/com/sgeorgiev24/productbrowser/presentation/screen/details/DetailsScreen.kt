ddd70ec4ab1d498009433db249ee6dba330d87ac
Stanislav Georgiev <sgeorgiev24@gmail.com>
2025-01-05 19:03:46 +0200

    Support landscape in the details

--- presentation/src/main/kotlin/com/sgeorgiev24/productbrowser/presentation/screen/details/DetailsScreen.kt
+++ presentation/src/main/kotlin/com/sgeorgiev24/productbrowser/presentation/screen/details/DetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.sgeorgiev24.productbrowser.presentation.screen.details
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -8,6 +9,7 @@ import coil3.load
 import com.sgeorgiev.productbrowser.presentation.databinding.DetailsViewBinding
 import org.koin.androidx.compose.koinViewModel
 
+@SuppressLint("NewApi")
 @Composable
 fun DetailsScreen() {
     val viewModel: DetailsViewModel = koinViewModel()
